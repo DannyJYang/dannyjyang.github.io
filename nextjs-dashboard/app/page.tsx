6fefae0701a5bc7dd0c753e1c1a6e3b616e7590d
dyang98 <jinhong.yang.mil@army.mil>
2026-04-27 18:43:34 -0500

    Back to square 1 - npm run dev works now

--- nextjs-dashboard/app/page.tsx
+++ nextjs-dashboard/app/page.tsx
@@ -21,7 +21,7 @@ export default function Page() {
                         , brought to you by Vercel.
                     </p>
                     <div
-                        className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"
+                        className="relative w-0 h-0 border-l-15 border-r-15 border-b-26 border-l-transparent border-r-transparent border-b-black"
                     />
                     <Link
                         href="/login"
